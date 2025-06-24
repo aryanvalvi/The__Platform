803aae17dfcada1f7ca84c430e0e2a158a46aeb7
aryanvalvi <aryanvalvi323@gmail.com>
2025-06-24 13:14:40 +0530

    70%project done

--- backend/server.js
+++ backend/server.js
@@ -95,5 +95,5 @@ mongoose
 // checkConnection()
 // retrive()
 app.listen(process.env.PORT || 5001, () => {
-  console.log("Server is running on port 5000")
+  console.log("Server is running on port 5001")
 })
