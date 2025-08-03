d9d46a2a2bc01d542656ada99d627b9dd4790ae3
aryanvalvi <aryanvalvi323@gmail.com>
2025-08-03 19:48:36 +0530

    bruh

--- backend/server.js
+++ backend/server.js
@@ -34,17 +34,17 @@ const client = require("./elasticSearchSync/sync")
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", req.headers.origin) // Dynamically allow origin
-//   res.header("Access-Control-Allow-Credentials", "true")
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   )
-//   next()
-// })
-//  "http://localhost:3000"
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin) // Dynamically allow origin
+  res.header("Access-Control-Allow-Credentials", "true")
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  )
+  next()
+})
+;("http://localhost:3000")
 const allowOriginsare = process.env.frontendUrl.split(",")
 app.use(
   cors({
